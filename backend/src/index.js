16fcdccdee195caff1a10330ea6c0668db1122fc
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-09-08 15:41:42 +0530

    custom api response and error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` ⚙ Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !! ", err);
+})
