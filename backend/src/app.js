d1cf2b217d97c4c57e2e5e04c81f9399bc1426ce
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-09-09 00:53:22 +0530

    cloudinary added and everything configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,5 +13,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app } 
