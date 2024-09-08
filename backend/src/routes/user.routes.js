8a305f948cd3386a5b8235fe2da40f5245a6c2cb
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-09-09 02:00:42 +0530

    user controller logic

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.single("avatar"),  
+    registerUser
+)
 
 export default router
