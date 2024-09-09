a65fad7be1e2bab29ecbab3e8f2d22098ec3bb69
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-09-09 23:35:01 +0530

    added auth middleware and login/logout controllers

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+
+export const verifyJWT = asyncHandler( async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if (!user) {
+            throw new ApiError(401, "Invalid access token")
+        }
+    
+        req.user = user;
+        next()
+
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+
+})
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -37,7 +37,7 @@ const userSchema = new Schema({
     },
     role: {
         type: String,
-        enum: ['student', 'admin'],
+        enum: ["student", "admin"],
         required: true
     },
     borrowHistory: [
