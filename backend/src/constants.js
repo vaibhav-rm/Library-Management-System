efcc9ca55fce77483dd3a53876f16cb0e4e353b5
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-10-07 23:15:35 +0530

    constants file updated and logout button added

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -3,3 +3,5 @@ export const LOAN_DURATION = 14;
 export const LIBRARY_NAME = "Government Polytechnic College Library";
 export const LATE_FEE = 0;
 export const MAX_BOOK_PER_STUDENT = 5;
+export const SMS_NOTIFICATIONS = false;
+export const EMAIL_NOTIFICATIONS = true;
